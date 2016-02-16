bea9eb008cc695b0dd00c8b7440580b479d74b28
Michael Meli <mjmeli@ncsu.edu>
2016-02-15 21:24:33 -0500

    preliminary frequency sweeping

--- rfduino/ad5933-test/ad5933-test.ino
+++ rfduino/ad5933-test/ad5933-test.ino
@@ -28,6 +28,14 @@ void setup(void)
 
   // Begin I2C
   Wire.begin();
+
+  // Set up
+  ad5933.reset();
+  ad5933.setInternalClock(true);
+  ad5933.setStartFrequency(30000);
+  ad5933.setIncrementFrequency(1000);
+  ad5933.setNumberIncrements(10);
+  ad5933.setPGAGain(PGA_GAIN_X1);
 }
 
 void loop(void)
@@ -35,7 +43,33 @@ void loop(void)
   // Test - get temperature
   double temp = ad5933.getTemperature();
   Serial.println(temp);
+
+  // Perform frequency sweep
+  ad5933.setPowerMode(POWER_STANDBY); // standby 
+  ad5933.setControlMode(CTRL_INIT_START_FREQ);
+  RFduino_ULPDelay(50);
+  ad5933.setControlMode(CTRL_START_FREQ_SWEEP);
+  RFduino_ULPDelay(50);
+  while (true) {
+    int real, imag;
+    if (ad5933.getComplexData(&real, &imag)) {
+      Serial.print("REAL=");
+      Serial.print(real);
+      Serial.print(" IMAG=");
+      Serial.println(imag);
+    } else {
+      Serial.println("Failed to get data");
+      break;
+    }
+    if ((ad5933.readStatusRegister() & STATUS_FREQ_SWEEP_DONE) == STATUS_FREQ_SWEEP_DONE) {
+      Serial.println("Frequency sweep completed");
+      break;
+    } else {
+      ad5933.setControlMode(CTRL_INCREMENT_FREQ);
+      RFduino_ULPDelay(50);
+    }
+  }
   
-  // Delay for a minute
-  RFduino_ULPDelay( SECONDS(5) );
+  // Delay
+  RFduino_ULPDelay( SECONDS(10) );
 }
