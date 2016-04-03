5cca48884666f029e8a1db26dc16e9eea4f34f56
Michael Meli <mjmeli@ncsu.edu>
2016-04-03 12:04:01 -0400

    updated MCP4018 library to take resistance AW instead of BW

--- rfduino/libraries/MCP4018/MCP4018.h
+++ rfduino/libraries/MCP4018/MCP4018.h
@@ -27,6 +27,10 @@
 #define I2C_RESULT_ADDR_NAK      (2)
 #define I2C_RESULT_DATA_NAK      (3)
 #define I2C_RESULT_OTHER_FAIL    (4)
+// Wiper resistance and resistance conversion
+#define MAX_RESISTANCE      (100000)
+#define STEP_RESISTANCE     ((float)MAX_RESISTANCE/127.0)
+#define WIPER_RESISTANCE    (150)   // this can vary and is a rough estimate
 
 /**
  * MCP4018 Library class
@@ -34,8 +38,18 @@
  */
 class MCP4018 {
     public:
-        // Set value between 0 and 128
+        // Set value between 0 and 127
         static bool setValue(byte);
+
+        // Set the potentiometer close to a resistance as possible
+        static bool setResistance(float);
+
+        // Get the value code in order to set the potentiometer as close to a
+        // resistance as possible
+        static byte getValueForResistance(float);
+
+        // Get the predicted resistance of the potentiometer for a value code
+        static float getResistanceForValue(byte);
 };
 
 #endif
